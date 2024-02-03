a8c2d3d25923b90293c2b4e5fdadd297f454d073
Andy Luis <info@triomni-it.com>
2024-02-03 18:26:04 +0000

    renamed jat just before deploying new version. This way doesnt fail CodeDeploy deployment with error "The deployment failed because a specified file already exists at this location: /tmp/spring-petclinic-3.2.0-SNAPSHOT.jar"

--- stop-process.sh
+++ stop-process.sh
@@ -3,3 +3,4 @@
 #Better way (instead of killing) would be to register a JVM or SpringBoot Actuator shutdown hook
 #for now, this wil do to demo DevOps auto deployment
 ps -ef | grep "spring-petclinic" | grep -v grep | cut -d' ' -f2 | xargs kill -9
+mv /tmp/spring-petclinic-3.2.0-SNAPSHOT.jar /tmp/spring-petclinic-3.2.0-SNAPSHOT.jar.prev
