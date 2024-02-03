2d42098e75b78d135f07d016338b3e01d653826d
Andy Luis <info@triomni-it.com>
2024-02-03 20:22:49 +0000

    disregard error when no proess is running which will fail deployment script when app not running (ex: first ever deployment)

--- stop-process.sh
+++ stop-process.sh
@@ -2,4 +2,4 @@
 
 #Better way (instead of killing) would be to register a JVM or SpringBoot Actuator shutdown hook
 #for now, this wil do to demo DevOps auto deployment
-ps -ef | grep "spring-petclinic" | grep -v grep | awk '{print $2}' | xargs kill -9
+ps -ef | grep "spring-petclinic" | grep -v grep | awk '{print $2}' | xargs kill -9 2>/dev/null
