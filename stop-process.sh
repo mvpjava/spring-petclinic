e9cd7e4eb7ad35b553c8b3303756cbb0134e55f0
Andy <info@triomni-it.com>
2024-02-05 15:58:45 +0000

    made stop-process.sh more robust as to not fail the CodeDeploy ApplicationStop phase

--- stop-process.sh
+++ stop-process.sh
@@ -2,4 +2,17 @@
 
 #Better way (instead of killing) would be to register a JVM or SpringBoot Actuator shutdown hook
 #for now, this wil do to demo DevOps auto deployment
-ps -ef | grep "spring-petclinic" | grep -v grep | awk '{print $2}' | xargs sudo kill -9 2>/dev/null
+
+# Find the process ID
+pid=$(ps -ef | grep "spring-petclinic" | grep -v grep | awk '{print $2}')
+
+# Check if the process ID exists
+if [ -z "$pid" ]; then
+    echo "No process found for 'spring-petclinic'. Exiting gracefully."
+    exit 0
+else
+    echo "Found process ID $pid for 'spring-petclinic'. Killing the process..."
+    sudo kill -9 "$pid"
+    echo "Process killed successfully."
+    exit 0
+fi
