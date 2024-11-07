cf595a7def1ca80eb3a01ae6ef8a8af406d78068
Andy Luis <mvpjava.email@gmail.com>
2024-11-07 13:26:22 +0000

    validated some syntax errors and re-worked directory locations

--- src/test/jmeter/run_jmeter_tests.sh
+++ src/test/jmeter/run_jmeter_tests.sh
@@ -10,20 +10,17 @@ JMETER_DOWNLOAD_BASE_FILENAME="apache-jmeter-$JMETER_VERSION"
 #Example: apache-jmeter-5.6.3.tgz
 JMETER_DOWNLOAD_FILENAME_TARBALL="$JMETER_DOWNLOAD_BASE_FILENAME.tgz"
 
-# Set JMeter home, if pre-installed on the instance
-export JMETER_HOME="/opt/$JMETER_DOWNLOAD_BASE_FILENAME"
-export PATH=$JMETER_HOME/bin:$PATH
-
 wget "https://downloads.apache.org//jmeter/binaries/$JMETER_DOWNLOAD_FILENAME_TARBALL"
-tar xzf "$JMETER_DOWNLOAD_FILENAME_TARBALL"
+tar xzf "$JMETER_DOWNLOAD_FILENAME_TARBALL" -C /opt
 
 # Run JMeter against the local application
-jmeter -n -t /opt/petclinic_test_plan.jmx -l /tmp/results.jtl -JTARGET_URL=http://localhost:8080
+/opt/$JMETER_DOWNLOAD_BASE_FILENAME/bin/jmeter -n -t /tmp/petclinic_test_plan.jmx -l /tmp/results.jtl -JTARGET_URL=http://localhost:8080
 
-# Check for errors in JMeter output and exit if any issues found
-if grep -q "error" /tmp/results.jtl; then
+# Check for errors in JMeter output and exit if any issues found.
+# Sending possible grep output to /dev/null as not interested in seeing it when executed manually
+if grep -E 'Non HTTP response message|Connection refused|Timeout|Internal Server Error' /tmp/results.jtl >/dev/null; then
   echo "JMeter tests encountered errors. Investigation needed"
-  #Will not exit for demo sake
+  #Will not exit for demo sake. Also best to use CloudWatch
   #exit 1
 fi
 
